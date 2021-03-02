a1ad9b050f5b6c53e6b3281059de74dff5cf905c
Simon Page <simon.page@krakentechnologies.ltd>
2021-03-02 12:38:43 +0000

    Rework to a dynamic set of policy documents

--- main.tf
+++ main.tf
@@ -1,26 +1,37 @@
 locals {
-  bucket_names = [
-    "bucket1",
-    "bucket2"
-  ]
-
-  bucket_arns = flatten([
-    for bucket_name in local.bucket_names : [
-      "aws:arn:aws:s3:::${bucket_name}",
-      "aws:arn:aws:s3:::${bucket_name}/*",
-    ]
-  ])
+  bucket_permission_maps = {
+    list_bucket = {
+      actions   = ["s3:ListBucket*"]
+      resources = ["bucket1", "bucket2"]
+    },
+    list_get_put_archive = {
+      actions   = ["s3:ListBucket*", "s3:GetObject*", "s3:PutObject*"]
+      resources = ["archive"]
+    }
+  }
 }
 
-output "policy_from_template" {
-  value = templatefile(
-    "example.json.tmpl",
-    {
-      bucket_arns_json = jsonencode(local.bucket_arns)
+data "aws_iam_policy_document" "policies" {
+  for_each = local.bucket_permission_maps
+
+  statement {
+    effect = "Allow"
+    resources = flatten([
+      for bucket_name in each.value.resources : [
+        "aws:arn:aws:s3:::${bucket_name}",
+        "aws:arn:aws:s3:::${bucket_name}/*",
+      ]
+    ])
+    actions = each.value.actions
+
+    condition {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+      values   = ["true"]
     }
-  )
+  }
 }
 
 output "policy_from_iam_policy_document" {
-  value = data.aws_iam_policy_document.policy.json
+  value = [for policy in data.aws_iam_policy_document.policies : policy]
 }
